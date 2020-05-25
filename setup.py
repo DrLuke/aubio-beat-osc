7f23cd7889e215417ec7975e8e0038fb3662fbdb
DrLuke <Lukas@Jackowski.de>
2020-05-25 18:57:23 +0200

    Add setup.py and pyproject.toml

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="aubio-beat-osc",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+          "wheel",
+    ],
+
+    author="Lukas Jackowski",
+    description="Simple beat detection outputting to OSC servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="aubio beat detection osc",
+
+    project_urls={
+        "Source Code": "https://github.com/DrLuke/aubio-beat-osc",
+    },
+
+    entry_points={
+        "console_scripts": [
+            "aubio-beat-osc=aubio_beat_osc:main",
+        ]
+    }
+)
