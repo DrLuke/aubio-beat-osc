7e43d435d7f284bd0781b052f3100cc7e7b0fc60
DrLuke <Lukas@Jackowski.de>
2020-05-25 20:03:17 +0200

    Fix setup.py to include module

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="aubio-beat-osc",
-    version="1.0",
-    packages=find_packages(),
+    version="1.1",
+    py_modules=["aubio_beat_osc"],
     install_requires=[
           "wheel",
     ],
